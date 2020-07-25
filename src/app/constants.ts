2abf345fd0f1f9e81b0642ce2b1acbe4271d2a8d
Abhijeet Singh <abhijeet.singh@navixel.com>
2020-07-25 18:09:32 +0530

    project added

---
+++ src/app/constants.ts
@@ -0,0 +1,6 @@
+export class AppSettings {
+//public static API_ENDPOINT= location.protocol + '//parrotautotrader.co.uk/server/public/api/';
+public static API_ENDPOINT= 'https://parrotautotrader.co.uk/server/public/api/';
+//public static API_ENDPOINT='http://localhost:8000/api/';
+}
+
